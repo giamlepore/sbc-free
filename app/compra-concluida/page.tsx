85551677c681c8ecfeab470812f2cce2f231f5de
Giampaolo <giam.lepore@gmail.com>
2024-12-11 12:00:40 -0300

    change index

--- app/compra-concluida/page.tsx
+++ app/compra-concluida/page.tsx
@@ -1,4 +1,9 @@
 import { PurchaseComplete } from '@/components/PurchaseComplete'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  robots: 'noindex, nofollow'
+}
 
 export default function PurchaseCompletePage() {
   return <PurchaseComplete />
